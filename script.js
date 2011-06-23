1fb3bdefc95b0bf6f49fe99ad883a3ee7cfc9d11
Victor Hallberg <vigge19@gmail.com>
2011-06-23 17:45:42 +0200

    Convert autocompleter into suggestion module and fix updating & highlighting

--- script.js
+++ script.js
@@ -1,24 +1,253 @@
 $(function() {
-	var data = ['auto', 'complete', 'test', 'example', 'examples', 'code',
-		'html', 'css', 'xhtml', 'js', 'javascript', 'json'];
+	// {{{ Suggestions
+	var Suggestions = function(input, data, opts) {
+		// Options
+		opts = $.extend({
+			listClass: 'sg_list',
+			activeClass: 'active',
+			limit: 4,
+			highlight: function(value, term) {
+				return value.replace(
+					new RegExp('(?!<[^<>]*)(' + regexEscape(term) + ')(?![^<>]*>)', 'gi'),
+					'<strong>$1</strong>');
+			}
+		}, opts);
+
+		var $input = $(input).attr('autocomplete', 'off');
+		var $list = $('<ul />').appendTo('body').hide()
+			.addClass(opts.listClass).css('position', 'absolute');
+
+		var hasFocus = 0;
+		var lastKey = -1;
+		var previousValue;
+
+		var selected = -1;
+		var listItems;
+
+		var blockSubmit = false;
+		var mouseDownOnList = false;
+		var timeout;
+
+		var KEY = {
+			LEFT: 37,
+			UP: 38,
+			RIGHT: 39,
+			DOWN: 40,
+			DEL: 46,
+			TAB: 9,
+			ENTER: 13,
+			ESC: 27,
+			COMMA: 188,
+			BACKSPACE: 8
+		};
+
+		$.browser.opera && $input.closest('form').bind('submit.sg', function() {
+			if (blockSubmit) {
+				blockSubmit = false;
+				return false;
+			}
+		});
+
+		// {{{ Input handlers
+		$input.bind('keydown.sg', function(event) {
+			hasFocus = 1;
+			lastKey = event.keyCode;
+			
+			switch (lastKey) {
+				case KEY.TAB:
+				case KEY.ENTER:
+					event.preventDefault();
+					selectCurrent();
+					return false;
+					break;
+
+				case KEY.UP:
+				case KEY.DOWN:
+					event.preventDefault();
+					moveSelected(lastKey == KEY.UP ? -1 : 1);
+					break;
+
+				case KEY.ESC:
+					event.preventDefault();
+					hide();
+					break;
+
+				case KEY.DEL:
+					onChange();
+					break;
+
+				default:
+					clearTimeout(timeout);
+					timeout = setTimeout(onChange, 100);
+			}
+		}).bind('focus.sg', function(event) {
+			hasFocus = 1;
+		}).bind('click.sg', function(event) {
+			if (hasFocus++ % 2 < 1 && !isVisible())
+				show();
+			else
+				hide();
+		}).bind('blur.sg', function() {
+			if (!mouseDownOnList)
+				hide();	
+		});
+		// }}}
+
+		// {{{ List handlers 
+		$list.mouseover(function(event) {
+			var target = targetLi(event);
+			if (!target) return;
+			selected = listItems.removeClass(opts.activeClass).index(target);
+			$(target).addClass(opts.activeClass);
+		}).click(function(event) {
+			listItems.removeClass(opts.activeClass);
+			var val = $(targetLi(event)).addClass(opts.activeClass).data('sg');
+			selectCurrent();
+			return false;
+		}).mousedown(function(event) {
+			mouseDownOnList = true;
+		}).mouseup(function(event) {
+			mouseDownOnList = false;
+		});
+		// }}}
+
+		function onChange() { // {{{
+			if (lastKey > 8 && lastKey < 32)
+				return sg.hide();
+
+			var current = input.val();
+			if (current == previousValue || lastKey == KEY.LEFT || lastKey == KEY.RIGHT)
+				return;
+
+			previousValue = current;
+			
+			if (current.length >= 1)
+				sg.show();
+			else
+				sg.hide();
+		} // }}}
+
+		function show() { // {{{
+			var offset = $input.offset();
+			$list.css({
+				'z-index': parseInt($input.css('zIndex')) + 1,
+				left: offset.left,
+				top: offset.top + $input.outerHeight() + 'px'
+			});
+
+			var val = $input.val();
+			console.log('show: input='+val);
+			var items = (val.length > 0) ? filter(data, val) : data;
+
+			renderList(items, val);
+
+			if (listItems.length) $list.show();
+			else $list.hide();
+		} // }}}
+
+		function hide() { // {{{
+			$list.hide();
+			listItems && listItems.removeClass(opts.activeClass);
+			selected = -1;
+		} // }}}
+
+		function currentSelection() { // {{{
+			console.log('currentSelection');
+			if (!isVisible()) return false;
+			console.log('visible');
+			var s = listItems && listItems.filter('.'+opts.activeClass)
+				.removeClass(opts.activeClass);
+			console.log([s, s.length, s.data(s[0], 'sg')]);
+			if (s && s.length) return $.data(s[0], 'sg');
+			return false;
+		} // }}}
+
+		function selectCurrent() { // {{{
+			if (!isVisible()) return false;
+			var sel = currentSelection();
+			hide();
+			if (!sel) return false;
+			$input.val(sel).focus();
+			return true;
+		} // }}}
+
+		function targetLi(event) { // {{{
+			var t = event.target;
+			while (t && t.tagName != 'LI')
+				t = t.parentNode;
+			if (!t || t.nodeName.toUpperCase() != 'LI' || t.className == 'more')
+				return false;
+			return t;
+		} // }}}
+
+		function renderList(items, term) { // {{{
+			console.log('renderList');
+			$list.empty();
+			var i = 0;
+			for (; i < items.length && i < opts.limit ; i++) {
+				var text = term ? opts.highlight(items[i], term) : items[i];
+				$('<li />').data('sg', items[i])
+					.append($('<a />').html(text))
+					.appendTo($list);
+			}
+			listItems = $list.find('li');
+
+			if (items.length > opts.limit)
+				$('<li class="more"></li>').appendTo($list);
+		} // }}}
+
+		function filter(array, term) { // {{{
+			var matcher = new RegExp(regexEscape(term));
+			return $.grep(array, function(value) {
+				return matcher.test(value.value || value);
+			});
+		} // }}}
+
+		function regexEscape(term) { // {{{
+			return term.replace(/([\^\$\(\)\[\]\{\}\*\.\+\?\|\\])/gi, "\\$1");
+		} // }}}
+
+		function moveSelected(step) { // {{{
+			if (!isVisible())
+				return show();
+
+			var length = listItems.length;
+			if (length < 1) return;
+
+			selected += step;
+			if (selected < 0)
+				selected = length - 1;
+			else if (selected >= length)
+				selected = 0;
+
+			listItems.removeClass(opts.activeClass)
+				.slice(selected, selected+1).addClass(opts.activeClass);
+		} // }}}
+
+		function isVisible() { // {{{
+			return $list.is(':visible');
+		} // }}}
+
+		// {{{ Returned
+		return {
+			show: show,
+			hide: hide,
+			prev: function() { moveSelected(-1); },
+			next: function() { moveSelected(1); },
+			selected: currentSelection,
+			visible: isVisible,
+			update: onChange
+		};
+		// }}}
+	};
+	// }}}
 
 	var input = $('#t');
 	var $container = $('<div class="tag-container">').insertAfter(input)
 		.click(function() { if (!input.is(':focus')) input.focus(); });
-	input.appendTo($container).attr('autocomplete', 'off');
+	input.appendTo($container);
 
-	var $list = $('<ul />').appendTo('body').hide()
-		.addClass('ac_list').css('position', 'absolute');
-
-	var activeClass = 'active';
-	var hasFocus = 0;
-	var lastKeyCode = null;
-	var prevValue = '';
-	var selected = -1;
-	var listItems;
-
-	var blockSubmit = false;
-
+	// {{{ addTag()
 	function addTag(name) {
 		$('<a href="#" class="tag"></a>').text(name)
 			.append($(' <span>x</span>')).click(function() {
@@ -28,6 +257,7 @@ $(function() {
 			.append($('<input name="tags[]" type="hidden" />').val(name))
 			.insertBefore(input);
 	}
+	// }}}
 
 	var existing = input.val().split(',');
 	if (existing) for (var i in existing)
@@ -36,94 +266,25 @@ $(function() {
 
 	input.removeAttr('name');
 
-	$.browser.opera && input.closest('form').bind('submit.ac', function() {
-		if (blockSubmit) {
-			blockSubmit = false;
-			return false;
-		}
-	});
+	var data = ['auto', 'complete', 'test', 'example', 'examples', 'code',
+		'html', 'css', 'xhtml', 'js', 'javascript', 'json'];
+	var sg = Suggestions(input, data);
 
 	input.keydown(function(e) {
-		hasFocus = 1;
-		lastKeyPressCode = e.keyCode;
-
+		console.log('tag keydown');
 		// Tab, space and comma adds the tag
 		switch (e.keyCode) {
 			case 9: // tab
-			case 32: // space 
-			case 13: // return
+			case 32: // space
 			case 188: // comma
 				e.preventDefault();
-				
-				// Insert completed value if list is visible
-				var completed = selectedValue();
-				if (completed) {
-					input.val(completed);
-					hide();
-				}
-
 				if (input.val().length > 0) {
 					addTag(input.val());
 					input.val('');
 				}
-
-				updateInputWidth();
-				return false;
-				break;
-
-			case 38: // up
-				e.preventDefault();
-				moveSelected(-1);
-				break;
-
-			case 40: // down
-				e.preventDefault();
-				moveSelected(1);
-				break;
-
-			case 27: // escape
-				e.preventDefault();
-				hide();
-				break;
-
-			case 46: // delete
-				input.change();
 		}
-
 		updateInputWidth();
-	}).focus(function() {
-		hasFocus = 1;
-	}).click(function() {
-		if (hasFocus++ % 2 < 1 && !isVisible())
-			show();
-		else
-			hide();
-
-		updateInputWidth();
-	}).blur(function() {
-		hasFocus = 0;
-		hide();
-
-		updateInputWidth();
-	}).change(function() {
-		if (lastKeyCode > 8 && lastKeyCode < 32)
-			return hide();
-		var v = input.val();
-		if (v == prevValue) return;
-	});
-
-	$list.mouseover(function(event) {
-		var target = targetLi(event);
-		if (!target) return;
-		selected = listItems.removeClass(activeClass).index(target);
-		$(target).addClass(activeClass);
-	}).click(function(event) {
-		listItems.removeClass(activeClass);
-		$(targetLi(event)).addClass(activeClass);
-		// select
-		input.focus();
-		return false;
-	});
+	}).click(updateInputWidth).blur(updateInputWidth);
 
 	function updateInputWidth() {
 		// Update input box width to match contents
@@ -135,77 +296,5 @@ $(function() {
 			wspan.remove();
 		}
 		input.css('width',  width);
-	}
-
-	function targetLi(event) {
-		var target = event.target;
-		while (target && target.tagName != 'LI')
-			target = target.parentNode;
-		if (!target || target.nodeName.toUpperCase() != 'LI') return false;
-		return target;
-	}
-
-	function filter(array, term) {
-		var matcher = new RegExp(term.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&"), 'i');
-		return $.grep(array, function(value) {
-			return matcher.test(value.value || value);
-		});
-	}
-
-	function renderList(items) {
-		console.log('renderList');
-		$list.empty();
-		$.each(items, function(i, item) {
-			return $('<li />').data('ac_value', item)
-				.append($('<a />').text(item)).appendTo($list);
-		});
-		listItems = $list.find('li');
-	}
-
-	function isVisible() {
-		return $list.is(':visible');
-	}
-
-	function show() {
-		var offset = input.offset();
-		$list.css({
-			'z-index': parseInt(input.css('zIndex')) + 1,
-			left: offset.left,
-			top: offset.top + input.outerHeight() + 'px'
-		});
-
-		var val = input.val();
-		var items = (val.length > 0) ? filter(data, val) : data;
-
-		renderList(items);
-		if (listItems.length) $list.show();
-	}
-
-	function hide() {
-		$list.hide();
-		listItems && listItems.removeClass(activeClass);
-		selected = -1;
-	}
-
-	function selectedValue() {
-		if (!isVisible()) return false;
-		var s = listItems && listItems.filter('.active').removeClass(activeClass);
-		return s && s.length && $.data(s[0], 'ac_value');
-	}
-
-	function moveSelected(step) {
-		if (!isVisible()) show();
-
-		var length = listItems.length;
-		if (length < 1) return;
-
-		selected += step;
-		if (selected < 0)
-			selected = length - 1;
-		else if (selected >= length)
-			selected = 0;
-
-		listItems.removeClass(activeClass)
-			.slice(selected, selected+1).addClass(activeClass);
 	}
 });
